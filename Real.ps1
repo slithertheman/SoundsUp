8421a6ff60722bc83c49acd8f4f25eecea06be6f
anon <101436008+slithertheman@users.noreply.github.com>
2022-12-25 17:06:55 -0600

    Update Real.ps1

--- Real.ps1
+++ Real.ps1
@@ -1,4 +1,60 @@
-$wav = "https://github.com/slithertheman/BadUSBDIR/blob/main/fnaf-4-jumpscare-but-its-earrape.wav?raw=true"
+$wav = "https://github.com/slithertheman/SoundsUp/blob/main/pibby-glitch-sound-effect-earrape-version.wav?raw=true"
+
+$w = -join($wav,"?dl=1")
+iwr $w -1 $env:TMP\s.wav
+
+function Play-WAV{
+$PlayWav=New-Object System.Media.SoundPlayer;$PlayWav.SoundLocation="$env:TMP\s.wav";$PlayWav.playsync()
+}
+
+#----------------------------------------------------------------------------------------------------
+
+# This turns the volume up to max level
+$k=[Math]::Ceiling(100/2);$o=New-Object -ComObject WScript.Shell;for($i = 0;$i -lt $k;$i++){$o.SendKeys([char] 175)}
+
+$wav = "https://github.com/slithertheman/SoundsUp/blob/main/pibby-glitch-sound-effect-earrape-version.wav?raw=true"
+
+$w = -join($wav,"?dl=1")
+iwr $w -1 $env:TMP\s.wav
+
+function Play-WAV{
+$PlayWav=New-Object System.Media.SoundPlayer;$PlayWav.SoundLocation="$env:TMP\s.wav";$PlayWav.playsync()
+}
+
+#----------------------------------------------------------------------------------------------------
+
+# This turns the volume up to max level
+$k=[Math]::Ceiling(100/2);$o=New-Object -ComObject WScript.Shell;for($i = 0;$i -lt $k;$i++){$o.SendKeys([char] 175)}
+
+$wav = "https://github.com/slithertheman/SoundsUp/blob/main/pibby-glitch-sound-effect-earrape-version.wav?raw=true"
+
+$w = -join($wav,"?dl=1")
+iwr $w -1 $env:TMP\s.wav
+
+function Play-WAV{
+$PlayWav=New-Object System.Media.SoundPlayer;$PlayWav.SoundLocation="$env:TMP\s.wav";$PlayWav.playsync()
+}
+
+#----------------------------------------------------------------------------------------------------
+
+# This turns the volume up to max level
+$k=[Math]::Ceiling(100/2);$o=New-Object -ComObject WScript.Shell;for($i = 0;$i -lt $k;$i++){$o.SendKeys([char] 175)}
+
+$wav = "https://github.com/slithertheman/SoundsUp/blob/main/pibby-glitch-sound-effect-earrape-version.wav?raw=true"
+
+$w = -join($wav,"?dl=1")
+iwr $w -1 $env:TMP\s.wav
+
+function Play-WAV{
+$PlayWav=New-Object System.Media.SoundPlayer;$PlayWav.SoundLocation="$env:TMP\s.wav";$PlayWav.playsync()
+}
+
+#----------------------------------------------------------------------------------------------------
+
+# This turns the volume up to max level
+$k=[Math]::Ceiling(100/2);$o=New-Object -ComObject WScript.Shell;for($i = 0;$i -lt $k;$i++){$o.SendKeys([char] 175)}
+
+$wav = "https://github.com/slithertheman/SoundsUp/blob/main/pibby-glitch-sound-effect-earrape-version.wav?raw=true"
 
 $w = -join($wav,"?dl=1")
 iwr $w -1 $env:TMP\s.wav
